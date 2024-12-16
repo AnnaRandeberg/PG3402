cdd40aada0a230c27ff38e1a441604438de0109b
Anna Randeberg <anna-randeberg@hotmail.com>
2024-12-16 18:19:15 +0100

    La til loginservice filer i prosjektet

---
+++ flashcardservice/src/main/java/org/quizapp/flashcardservice/FlashcardserviceApplication.java
@@ -0,0 +1,13 @@
+package org.quizapp.flashcardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashcardserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashcardserviceApplication.class, args);
+	}
+
+}
---
+++ flashcardservice/src/test/java/org/quizapp/flashcardservice/FlashcardserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.quizapp.flashcardservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashcardserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
