55aca0b5683231be52708c3f86e3cb6b11abec85
Anna Randeberg <anna-randeberg@hotmail.com>
2024-12-16 15:34:54 +0100

    changed quizid to title

--- quizservice/src/main/resources/db/migration/V005__insert_data.sql
+++ quizservice/src/main/resources/db/migration/V005__insert_data.sql
@@ -1,17 +1,17 @@
-INSERT INTO quiz (quiz_id, title, chapter, subject)
-SELECT 1, 'Matte 8. klasse', 'Algebra', 'Mattematikk'
+INSERT INTO quiz (title, chapter, subject)
+SELECT 'Matte 8. klasse', 'Algebra', 'Mattematikk'
 WHERE NOT EXISTS (
-    SELECT 1 FROM quiz WHERE quiz_id = 1
+    SELECT 1 FROM quiz WHERE title = 'Matte 8. klasse'
 );
 
-INSERT INTO questions (question_id, quiz_id, question_text, answer)
-SELECT 1, 1, 'Hva er 2 + 2?', '4'
-WHERE NOT EXISTS (
-    SELECT 1 FROM questions WHERE question_id = 1
-);
 
-INSERT INTO questions (question_id, quiz_id, question_text, answer)
-SELECT 2, 1, 'Hva er 3 * 3?', '9'
+INSERT INTO questions (quiz_id, question_text, answer)
+SELECT 1, 'Hva er 2 + 2?', '4'
 WHERE NOT EXISTS (
-    SELECT 1 FROM questions WHERE question_id = 2
-);
+    SELECT 1 FROM questions WHERE question_text = 'Hva er 2 + 2?');
+
+
+INSERT INTO questions (quiz_id, question_text, answer)
+SELECT 1, 'Hva er 3 * 3?', '9'
+WHERE NOT EXISTS (
+    SELECT 1 FROM questions WHERE question_text = 'Hva er 3 * 3?');
