2be7672847d32f4927bb9928ba2e29d1e2188057
Anna Randeberg <anna-randeberg@hotmail.com>
2024-12-18 01:41:08 +0100

    removed unused imports and methods

--- quizservice/src/main/java/org/quizapp/quizservice/configurations/AmqpConfigurations.java
+++ quizservice/src/main/java/org/quizapp/quizservice/configurations/AmqpConfigurations.java
@@ -1,18 +1,11 @@
 /*soure:https://github.com/bogdanmarculescu/microservices2024/blob/main/ongoing/src/main/java/org/cards/ongoinground/configurations/AmqpConfiguration.java*/
 
 package org.quizapp.quizservice.configurations;
-
-import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
-import org.springframework.amqp.rabbit.annotation.RabbitListenerConfigurer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.converter.MappingJackson2MessageConverter;
-import org.springframework.messaging.handler.annotation.support.DefaultMessageHandlerMethodFactory;
-import org.springframework.messaging.handler.annotation.support.MessageHandlerMethodFactory;
-import com.fasterxml.jackson.annotation.JsonCreator;
 
 @Configuration
 public class AmqpConfigurations {
@@ -38,19 +31,6 @@ public class AmqpConfigurations {
     public Jackson2JsonMessageConverter producerJackson2MessageConverter() {
         return new Jackson2JsonMessageConverter();
     }
-/*
-    @Bean
-    public MessageHandlerMethodFactory messageHandlerMethodFactory() {
-        DefaultMessageHandlerMethodFactory factory = new DefaultMessageHandlerMethodFactory();
-        MappingJackson2MessageConverter jsonConverter = new MappingJackson2MessageConverter();
-        jsonConverter.getObjectMapper().registerModule(new ParameterNamesModule(JsonCreator.Mode.PROPERTIES));
-        factory.setMessageConverter(jsonConverter);
-        return factory;
-    }
 
-    @Bean
-    public RabbitListenerConfigurer rabbitListenerConfigurer(MessageHandlerMethodFactory messageHandlerMethodFactory) {
-        return (c) -> c.setMessageHandlerMethodFactory(messageHandlerMethodFactory);
-    }*/
 
 }
