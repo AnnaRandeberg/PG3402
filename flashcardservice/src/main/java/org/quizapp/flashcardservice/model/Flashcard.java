4c4218fe7f321ebc3b76709c60adc442c9537597
danaithb <daha025@student.kristiania.no>
2024-12-18 03:42:32 +0100

    insert data funcunality in flashcard

--- flashcardservice/src/main/java/org/quizapp/flashcardservice/model/Flashcard.java
+++ flashcardservice/src/main/java/org/quizapp/flashcardservice/model/Flashcard.java
@@ -1,14 +1,26 @@
 package org.quizapp.flashcardservice.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Entity
 @Table(name = "flashcards")
+@Getter
+@Setter
 public class Flashcard {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonProperty("flashcardId")
     private Long flashcardId;
 
+    @Column(nullable = false)
+    @JsonProperty("questionText")
     private String questionText;
+
+    @Column(nullable = false)
+    @JsonProperty("answer")
     private String answer;
 }
